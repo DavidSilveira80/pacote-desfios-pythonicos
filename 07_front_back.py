5d31228f8e80cd91b7e669fd9ff3363790cc046f
DavidSilveira80 <davidpscontato@gmail.com>
2025-04-07 22:45:04 -0300

    07_front_back.py

--- 07_front_back.py
+++ 07_front_back.py
@@ -10,9 +10,47 @@ Exemplo: 'abcde', a metade da frente é 'abc' e a de trás é 'de'.
 Finalmente, dadas duas strings a e b, retorne uma string na forma:
 a-frente + b-frente + a-trás + b-trás
 """
+
+def retorna_meio_da_string(a, b):
+    meio_a = len(a) // 2
+    meio_b = len(b) // 2
+
+    return (meio_a, meio_b)
+
 def front_back(a, b):
     # +++ SUA SOLUÇÃO +++
-    return
+    if len(a) % 2 == 0:
+        if len(b) % 2 == 0:
+            meio_string_a, meio_string_b = retorna_meio_da_string(a, b)
+
+            frente_a = a[:meio_string_a]
+            tras_a = a[meio_string_a:]
+
+            frente_b = b[:meio_string_b]
+            tras_b = b[meio_string_b:]
+            resp = f'{frente_a}{frente_b}{tras_a}{tras_b}'
+        elif len(b) % 2 != 0:
+            meio_string_a, meio_string_b = retorna_meio_da_string(a, b)
+
+            frente_a = a[:meio_string_a]
+            tras_a = a[meio_string_a:]
+
+            frente_b = b[:meio_string_b + 1]
+            tras_b = b[meio_string_b + 1:]
+
+            resp = f'{frente_a}{frente_b}{tras_a}{tras_b}'
+    elif len(a) % 2 != 0 and len(b) % 2 != 0:
+        meio_string_a, meio_string_b = retorna_meio_da_string(a, b)
+
+        frente_a = a[:meio_string_a + 1]
+        tras_a = a[meio_string_a + 1:]
+
+        frente_b = b[:meio_string_b + 1]
+        tras_b = b[meio_string_b + 1:]
+
+        resp = f'{frente_a}{frente_b}{tras_a}{tras_b}'
+
+    return resp
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
